d41d6ee795fd0b69d3caf94cfe0eba89ba3e10e8
chengyi <732345907@qq.com>
2019-03-27 18:48:34 +0800

    尾部管理 待测试

---
+++ application/tuanj/controller/Footer.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/27
+ * Time: 18:35
+ */
+namespace app\tuanj\controller;
+
+use think\Db;
+use controller\BasicAdmin;
+use service\DataService;
+
+class Footer extends BasicAdmin {
+
+    private $dataform = 'foot_url';
+
+    public function index() {
+        $this->title = '底部导航设置';
+        list($get, $db) = [$this->request->get(), Db::name($this->dataform)];
+        (isset($get['keywords']) && $get['keywords'] !== '') && $db->whereLike('title|id', "%{$get['keywords']}%");
+        if (isset($get['date']) && $get['date'] !== '') {
+            list($start, $end) = explode(' - ', $get['date']);
+            $db->whereBetween('time', ["{$start} 00:00:00", "{$end} 23:59:59"]);
+        }
+        return parent::_list($db->order('id desc'));
+    }
+
+
+    //关联底部分类
+    protected function _data_filter(&$data) {
+        foreach ($data as $key => $val) {
+            if($val['fid'] ==1){
+                $data[$key]['footer_cates']='产品';
+            }else if($val['fid'] ==2){
+                $data[$key]['footer_cates']='顾客';
+            }else{
+                $data[$key]['footer_cates']='快速访问';
+            }
+        }
+    }
+
+
+    /**
+     * 添加
+     * @return type
+     */
+    public function add() {
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 编辑
+     * @return type
+     */
+    public function edit() {
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 添加成功回跳处理
+     * @param bool $result
+     */
+    protected function _form_result($result) {
+        if ($result !== false) {
+            list($base, $spm, $url) = [url('@admin'), $this->request->get('spm'), url('tuanj/footer/index')];
+            $this->success('数据保存成功！', "{$base}#{$url}?spm={$spm}");
+        }
+    }
+
+    /**
+     * 删除
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function del() {
+        if (DataService::update($this->dataform)) {
+            $this->success("删除成功!", '');
+        }
+        $this->error("删除失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function forbid() {
+        if (DataService::update($this->dataform)) {
+            $this->success("禁用成功!", '');
+        }
+        $this->error("禁用失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function resume() {
+        if (DataService::update($this->dataform)) {
+            $this->success("启用成功!", '');
+        }
+        $this->error("启用失败, 请稍候再试!");
+    }
+
+}
